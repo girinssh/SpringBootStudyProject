ed0c2c473715a4117ee64b547a0e4b98ec9a76d1
girinssh <tngus5851@naver.com>
2024-05-08 22:30:58 +0900

    add missings

---
+++ src/test/java/com/example/springbootstudyproject/projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstudyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class projectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
