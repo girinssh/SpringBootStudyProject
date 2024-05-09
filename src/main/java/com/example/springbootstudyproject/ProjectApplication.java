636c4319e415b06f8df9ac77e2d7627a3f52cd80
girinssh <tngus5851@naver.com>
2024-05-09 22:31:21 +0900

    add HelloController and its Tests.

--- src/main/java/com/example/springbootstudyproject/ProjectApplication.java
+++ src/main/java/com/example/springbootstudyproject/ProjectApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class projectApplication {
+public class ProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(projectApplication.class, args);
+        SpringApplication.run(ProjectApplication.class, args);
     }
-
 }
